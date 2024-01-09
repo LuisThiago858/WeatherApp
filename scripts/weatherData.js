1f0eb5deadc865c2128fadb3e7071f51f974a005
LuisThiago858 <luisrabello.ltsr@gmail.com>
2024-01-09 11:35:43 -0400

    -

--- scripts/weatherData.js
+++ scripts/weatherData.js
@@ -1,12 +1,86 @@
-import { clearForecastContainer, get5DayForecast } from "./forecast";
-
-export function mySearch(){
+function mySearch(){
   
   var inputValue=document.querySelector('#myInput').value;
   const city_name=inputValue;
   const api_key = "09cbafb7a5f7a20c8584da68c2163415";
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city_name}&appid=${api_key}`;
   
+  function kelvinToCelsius(tempKelvin){
+    return tempKelvin - 273.15;
+  }
+
+  function speedKmh(tempSpeed){
+      return tempSpeed * 3.6;
+  }
+
+  function gustKmh(tempGust){
+      return tempGust * 3.6;
+  }
+
+  function getDayOfWeek(){
+      const today = new Date();
+      return today.getDay();
+  }
+
+  function clearForecastContainer() {
+    const forecastContainer = document.getElementById('fftw');
+    forecastContainer.innerHTML = ''; // Limpar o conteúdo do contêiner
+  }
+  
+  function get5DayForecast(forecasts) {
+      const dayOfWeek = getDayOfWeek();
+      const result = {};
+    
+      forecasts.forEach((item) => {
+        const expectedDate = new Date(item.dt * 1000);
+        const day = expectedDate.getDay();
+    
+        if (day === dayOfWeek) {
+          
+          if (!result.today) {
+            result.today = {
+              temp: kelvinToCelsius(item.main.temp).toFixed(0),
+              wind: {
+                speed: speedKmh(item.wind.speed).toFixed(1),
+                deg: item.wind.deg,
+                gust: gustKmh(item.wind.gust).toFixed(1),
+              },
+              weather:{
+                main: item.weather[0].main,
+                description: item.weather[0].description,
+                icon:item.weather[0].icon,
+              },
+            };
+          }
+        }
+    
+        if (day !== dayOfWeek && day > dayOfWeek) {
+          const dayName = expectedDate.toLocaleDateString("pt-BR", {day:'2-digit', month:'2-digit'});
+          if (!result[dayName]) {
+            result[dayName] = {
+              tempMin: Infinity,
+              tempMax: -Infinity,
+            };
+          }
+    
+          const tempMin = kelvinToCelsius(item.main.temp_min);
+          const tempMax = kelvinToCelsius(item.main.temp_max);
+    
+          if (tempMin < result[dayName].tempMin) {
+            result[dayName].tempMin = tempMin;
+          }
+    
+          if (tempMax > result[dayName].tempMax) {
+            result[dayName].tempMax = tempMax;
+          }
+    
+        }
+      });
+  
+    return result;
+  }
+
+
   fetch(url)
     .then((response) => {
       if (!response.ok) {
@@ -162,6 +236,8 @@ export function mySearch(){
       console.error("Houve um problema: ", error);
     });
 }
+
+
 
 
 
